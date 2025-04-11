03e6117a51497049fe347c2f1162bfb9ed35750c
Amir Hossein <a.qorbani.eng@gmail.com>
2025-04-11 18:24:43 +0330

    change vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      manifest: {
+        name: 'Rick and Morty App',
+        short_name: 'R&M',
+        description: 'A Rick and Morty character browser',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/img/rm.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/index.html',
+      },
+    }),
+  ],
+});
