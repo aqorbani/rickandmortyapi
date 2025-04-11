844c0467f533c292c7170a792e2c811476561d38
Amir Hossein <a.qorbani.eng@gmail.com>
2025-04-11 18:46:56 +0330

    disable strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -9,26 +8,24 @@ import { Provider } from "react-redux";
 import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </ApolloProvider>
-  </StrictMode>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>
 );
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register("/service-worker.js")
       .then((registration) => {
-        console.log('✅ Service Worker registered: ', registration);
+        console.log("✅ Service Worker registered: ", registration);
       })
       .catch((error) => {
-        console.error('❌ Service Worker registration failed: ', error);
+        console.error("❌ Service Worker registration failed: ", error);
       });
   });
 }
